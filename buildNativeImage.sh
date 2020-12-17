8ac282c2d1b84f97b79d59a231e199f230f7a6f1
root <root@3c6514bb27ef>
2020-12-17 06:22:26 +0000

    add container packaging

--- buildNativeImage.sh
+++ buildNativeImage.sh
@@ -1,4 +1,5 @@
-if java -version | grep -q "java version" ; then
+#!/bin/bash
+if [ -n `which java` ]; then
   echo "Java installed."
 else 
   echo "Installing Java"
@@ -7,13 +8,13 @@ fi
 
 if [ -z "$JAVA_HOME" ]
 then
-  echo "set \$JAVA_HOME"
+  echo "set JAVA_HOME"
   echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" > /etc/profile.d/java.sh
   source /etc/profile.d/java.sh
 fi
 
-if mvn -version | grep -q "maven version" ; then
-  echo Maven installed."
+if [ -n `which mvn` ]; then
+  echo "Maven installed."
 else
   echo "Installing Maven"
   sudo apt install  -y maven
@@ -21,11 +22,9 @@ fi
 
 if [ -z "$MAVEN_HOME" ]
 then
-  echo "setting \$MAVEN_HOME"
+  echo "setting MAVEN_HOME"
   echo "export MAVEN_HOME=/usr/share/maven" > /etc/profile.d/maven.sh
   source /etc/profile.d/maven.sh
 fi
-
-./mvnw package -Pnative -Dquarkus.native.container-build=true
-
-
+sudo chown a+x mvnw
+./mvnw package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true
