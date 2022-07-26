b7d5cbc93aa16641b689bb633f5cabb4288fd915
CrimSol <47455550+CrimSol@users.noreply.github.com>
2022-07-26 17:09:05 +0200

    added comments

--- src/main/java/net/manaty/sergent/AgentResource.java
+++ src/main/java/net/manaty/sergent/AgentResource.java
@@ -27,6 +27,12 @@ public class AgentResource {
     @Inject
     AgentService service;
 
+    /**
+     * GET Request to Sergent Service
+     * Executes a specific script depending on command passed
+     * @param command Query Parameter => command to execute which executes a specific script
+     * @param params Query Parameter => Input of type Container/ File/ Realm/ fullUrl/ theme for deploy-kc-theme 
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String sergent(
@@ -64,6 +70,13 @@ public class AgentResource {
         return result;
     }
 
+    /**
+     * POST Request to Sergent Service
+     * @param command Command to execute 
+     * @param params String of parameters to pass with command
+     * @param timeoutSec Delay before timeout, default is 10 sec
+     * @return
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -109,6 +122,11 @@ public class AgentResource {
         return result;
     }
 
+    /**
+     * Execute command with no additional params
+     * @param command Command to execute
+     * @return
+     */
     private String execute(String command) {
         String result = null;
         service.setCommand(command);
@@ -121,6 +139,12 @@ public class AgentResource {
         return result;
     }
 
+    /**
+     * Execute command with params as options of command
+     * @param command Command to execute
+     * @param params Key-Value Options for specified command
+     * @return
+     */
     private String execute(String command, String params) {
         String result = null;
         service.setCommand(command);
@@ -133,6 +157,12 @@ public class AgentResource {
         return result;
     }
 
+    /**
+     * Execute command with multiple parameters
+     * @param command Command to execute
+     * @param params Additional Options to add to command
+     * @return
+     */
     private String executeMult(String command, String...params) {
         String result = null;
         service.setCommand(command);
